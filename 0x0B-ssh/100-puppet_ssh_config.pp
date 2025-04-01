0e0d7e3d412d8b4f786b87109dd19fb6d3a71dd2
Coder3451 <segniwoldemichael@gmail.com>
2025-04-01 19:41:50 +0300

    Update the config

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,17 +1,13 @@
 file_line { 'Turn off passwd auth':
-	path => '/etc/ssh/sshd_config',
-	line => 'PasswordAuthentication no',
-	match => '^#PasswordAuthentication',
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
+  match  => 'PasswordAuthentication',
 }
 
 file_line { 'Declare identity file':
-	path => '/etc/ssh/ssh_config',
-	line => 'IdentityFile ~/.ssh/school',
-	match => '^#IdentityFile',
-}
-
-file_line { 'Use publickey for auth':
-	path => '/etc/ssh/ssh_config',
-	line => 'PreferredAuthentications publickey',
-	match => '^#PreferredAuthentications',
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentityFile ~/.ssh/school',
+  match  => 'IdentityFile',
 }
